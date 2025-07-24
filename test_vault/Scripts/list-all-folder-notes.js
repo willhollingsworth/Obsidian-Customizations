6726f2e83d2273be1511d4f178345fb8643109f1
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-07-24 17:22:15 +0930

    updating comments and removing unused code

--- test_vault/Scripts/list-all-folder-notes.js
+++ test_vault/Scripts/list-all-folder-notes.js
@@ -1,19 +1,15 @@
-// List all folder notes in the current folder
+// List all folder notes and their path
 (async () => {
     const pages = dv.pages();
     const currentFolder = dv.current().file.folder;
 
     const matchingNotes = pages.filter((p) => {
-        // Only consider notes in subfolders of currentFolder
-        if (!p.file.folder.startsWith(currentFolder + '/')) return false;
-
         // Get the direct subfolder name (last part of the folder path)
         const subfolderName = p.file.folder.split('/').pop();
 
         return p.file.name === subfolderName;
     });
 
-    dv.paragraph(`Folder notes in folder ${currentFolder}: `);
     dv.table(
         ['Name', 'Path'],
         matchingNotes.map((p) => [dv.fileLink(p.file.path), p.file.folder])
